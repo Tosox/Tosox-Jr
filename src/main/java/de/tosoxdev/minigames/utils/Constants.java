c597d599d85b4022444d632dcbd095a080edc87a
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-05-24 07:59:19 +0200

    fix: hide bot token

--- src/main/java/de/tosoxdev/minigames/utils/Constants.java
+++ src/main/java/de/tosoxdev/minigames/utils/Constants.java
@@ -2,6 +2,6 @@ package de.tosoxdev.minigames.utils;
 
 public class Constants {
     public static final String BOT_NAME = "Minigames";
-    public static final String BOT_PREFIX = "!";
-    public static final String BOT_TOKEN = "ODUzNzUyNDczMzY1MjUwMDg5.YMZ88w.q0Gcj8TBcMdpsuUqmxtTzamQhfM";
+    public static final String BOT_PREFIX = "$";
+    public static final String BOT_TOKEN = System.getenv("MINIGAMES_TOKEN");
 }
