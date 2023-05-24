d7e46c66c8428f35acd4407950dc30ed654f3d80
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-05-24 08:00:09 +0200

    feat: rework ping command

--- src/main/java/de/tosoxdev/minigames/commands/ping/PingCmd.java
+++ src/main/java/de/tosoxdev/minigames/commands/ping/PingCmd.java
@@ -7,8 +7,7 @@ import net.dv8tion.jda.api.JDA;
 public class PingCmd implements ICommand {
     @Override
     public void handle(CommandContext commandContext) {
-        JDA jda = commandContext.getJDA();
-        jda.getRestPing().queue((ping) -> commandContext.getChannel().sendMessage("This little maneuver is gonna cost us " + ping + " ms").queue());
+        commandContext.getChannel().sendMessage("pong!").queue();
     }
 
     @Override
@@ -18,6 +17,6 @@ public class PingCmd implements ICommand {
 
     @Override
     public String getHelp() {
-        return "Shows current ping of the bot to the discord server";
+        return "Pong!";
     }
 }
