4a86b8eec2fb97a2e102c2c57c4f5800d1445ad3
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-06-21 17:04:52 +0200

    fix: new env var name

--- src/main/java/de/tosoxdev/tosoxjr/utils/Constants.java
+++ src/main/java/de/tosoxdev/tosoxjr/utils/Constants.java
@@ -3,5 +3,5 @@ package de.tosoxdev.tosoxjr.utils;
 public class Constants {
     public static final String BOT_NAME = "Tosox Jr";
     public static final String BOT_PREFIX = "$";
-    public static final String BOT_TOKEN = System.getenv("MINIGAMES_TOKEN");
+    public static final String BOT_TOKEN = System.getenv("TOSOXJR_TOKEN");
 }
