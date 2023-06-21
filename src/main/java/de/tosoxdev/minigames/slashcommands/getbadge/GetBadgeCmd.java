3e24c98918059e6aed50d8c073639f8f61be8168
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-06-21 09:57:32 +0200

    fix: null check

--- src/main/java/de/tosoxdev/minigames/slashcommands/getbadge/GetBadgeCmd.java
+++ src/main/java/de/tosoxdev/minigames/slashcommands/getbadge/GetBadgeCmd.java
@@ -1,15 +1,23 @@
-package de.tosoxdev.minigames.slashcommands.badge;
+package de.tosoxdev.minigames.slashcommands.getbadge;
 
 import de.tosoxdev.minigames.slashcommands.ISlashCommand;
 import de.tosoxdev.minigames.slashcommands.SlashCommandContext;
+import net.dv8tion.jda.api.entities.Guild;
 
 public class GetBadgeCmd implements ISlashCommand {
     @Override
     public void handle(SlashCommandContext slashCommandContext) {
-        String ownerTag = slashCommandContext.getGuild().getOwnerId();
-        String userTag = slashCommandContext.getAuthor().getId();
+        Guild guild = slashCommandContext.event().getGuild();
+        if (guild == null) {
+            System.out.println("[ERROR]: The guild was null when trying to run 'get-badge'");
+            return;
+        }
+
+        String ownerTag = slashCommandContext.event().getGuild().getOwnerId();
+        String userTag = slashCommandContext.event().getUser().getId();
         if (!userTag.equalsIgnoreCase(ownerTag)) {
             slashCommandContext.event().reply("You don't have the permission to run this command").queue();
+            return;
         }
 
         slashCommandContext.event().reply("The command was successfully executed :)").queue();
@@ -21,7 +29,7 @@ public class GetBadgeCmd implements ISlashCommand {
     }
 
     @Override
-    public String getHelp() {
+    public String getDescription() {
         return "Needs to be run at least one time a month to keep the 'Active Developer' badge";
     }
 }
