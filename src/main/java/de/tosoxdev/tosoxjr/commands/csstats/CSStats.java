e3ccc566925c76e91bd918a87ccc5f8bb64ba346
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-06-23 18:31:31 +0200

    refactor: make steam web api key global

--- src/main/java/de/tosoxdev/tosoxjr/commands/csstats/CSStats.java
+++ src/main/java/de/tosoxdev/tosoxjr/commands/csstats/CSStats.java
@@ -1,11 +1,14 @@
 package de.tosoxdev.tosoxjr.commands.csstats;
 
 import de.tosoxdev.tosoxjr.utils.APIRequest;
+import de.tosoxdev.tosoxjr.utils.Constants;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class CSStats {
-    private static final String STEAM_API_KEY = System.getenv("STEAM_API_KEY");
+    private static final String API_VANITY_URL = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=%s&vanityurl=%s";
+    private static final String API_GAME_STATS = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/?appid=%s&key=%s&steamid=%s";
+    private static final String API_USER_PROFILE = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s";
     private static final String CSGO_APP_ID = "730";
 
     private boolean isID64(String userid) {
@@ -22,7 +25,7 @@ public class CSStats {
             return userid;
         }
 
-        String query = String.format("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=%s&vanityurl=%s", STEAM_API_KEY, userid);
+        String query = String.format(API_VANITY_URL, Constants.STEAM_API_KEY, userid);
         JSONObject response = APIRequest.getJson(query);
         if ((response == null) || (response.isEmpty())) {
             return null;
@@ -39,7 +42,7 @@ public class CSStats {
     }
 
     public JSONObject getStatistics(String userid64) {
-        String query = String.format("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/?appid=%s&key=%s&steamid=%s", CSGO_APP_ID, STEAM_API_KEY, userid64);
+        String query = String.format(API_GAME_STATS, CSGO_APP_ID, Constants.STEAM_API_KEY, userid64);
         return APIRequest.getJson(query);
     }
 
@@ -58,7 +61,7 @@ public class CSStats {
     }
 
     public JSONObject getProfileInfos(String userid64) {
-        String query = String.format("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s", STEAM_API_KEY, userid64);
+        String query = String.format(API_USER_PROFILE, Constants.STEAM_API_KEY, userid64);
         JSONObject response = APIRequest.getJson(query);
         if (response == null) {
             return null;
--- src/main/java/de/tosoxdev/tosoxjr/utils/Constants.java
+++ src/main/java/de/tosoxdev/tosoxjr/utils/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String BOT_NAME = "Tosox Jr";
     public static final String BOT_PREFIX = "$";
     public static final String BOT_TOKEN = System.getenv("TOSOXJR_TOKEN");
+
+    public static final String STEAM_API_KEY = System.getenv("STEAM_API_KEY");
 }
