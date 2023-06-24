1706fced1654f227d57943ee8382664947b0f7ca
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-06-24 15:04:38 +0200

    feat: add dictionary api web key

--- src/main/java/de/tosoxdev/tosoxjr/utils/Constants.java
+++ src/main/java/de/tosoxdev/tosoxjr/utils/Constants.java
@@ -6,4 +6,5 @@ public final class Constants {
 
     public static final String BOT_TOKEN = System.getenv("TOSOXJR_TOKEN");
     public static final String STEAM_API_KEY = System.getenv("STEAM_API_KEY");
+    public static final String DICTIONARY_API_KEY = System.getenv("DICTIONARY_API_KEY");
 }
