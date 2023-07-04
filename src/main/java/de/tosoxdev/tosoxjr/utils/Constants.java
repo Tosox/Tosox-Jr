2fb9b76da0000a39db89e5d866dce0c5fdb3f0e2
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-07-04 06:23:23 +0200

    feat: production bot token

--- src/main/java/de/tosoxdev/tosoxjr/utils/Constants.java
+++ src/main/java/de/tosoxdev/tosoxjr/utils/Constants.java
@@ -2,9 +2,7 @@ package de.tosoxdev.tosoxjr.utils;
 
 public final class Constants {
     public static final String BOT_NAME = "Tosox Jr";
-    public static final String BOT_PREFIX = "$";
-
-    public static final String BOT_TOKEN = System.getenv("DEV_TOSOXJR_TOKEN"); // TODO
+    public static final String BOT_TOKEN = System.getenv("TOSOXJR_TOKEN");
     public static final String STEAM_API_KEY = System.getenv("STEAM_API_KEY");
     public static final String DICTIONARY_API_KEY = System.getenv("DICTIONARY_API_KEY");
 }
