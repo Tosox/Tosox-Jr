9a9fc86d4a756af62a65f7707c659f0d9f27bc1e
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-07-13 13:49:39 +0200

    feat: lowercase scramble words

--- src/main/java/de/tosoxdev/tosoxjr/commands/scramble/Scramble.java
+++ src/main/java/de/tosoxdev/tosoxjr/commands/scramble/Scramble.java
@@ -57,8 +57,15 @@ public class Scramble {
             return false;
         }
 
+        // Bugfix for the words with spaces ("Ansicht")
+        word = word.replaceAll("\\s+","");
+
         // Shuffle word
-        List<String> chars = new ArrayList<>(word.chars().mapToObj(c -> String.valueOf((char) c)).toList());
+        List<String> chars = new ArrayList<>(word
+                .toLowerCase()
+                .chars()
+                .mapToObj(c -> String.valueOf((char) c))
+                .toList());
         Collections.shuffle(chars);
         scrambledWord = String.join("", chars);
 
